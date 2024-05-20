79d6e08bd25b244a5ec09667e0a2b5470f3c08a7
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-20 18:04:56 +0200

    Add props and template to LevelComponent.vue

--- src/components/LevelComponent.vue
+++ src/components/LevelComponent.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+  import type { Level } from '@/models/Level'
+  import CardList from '@/components/CardList.vue'
+  import type { Card } from '@/models/Card'
 
+  const props = defineProps<{ level: Level, themeId: number }>();
+  const cards: Card[] = [];
 </script>
 
 <template>
-
+  <CardList :cards="level.cards"
+            :levelId="level.id"
+            :themeId="themeId"
+  ></CardList>
 </template>
 
 <style scoped>
