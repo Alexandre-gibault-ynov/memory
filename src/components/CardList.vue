419634e370e3ba99daccff19603d4f264bf2021e
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-20 15:19:30 +0200

    Add list of Card. function to add, delete, a card on the list. function to get a random card of the list.

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import type {Card} from '@/models/Card';
+import CardItem from '@/components/CardItem.vue';
 
+const cards: Card [] = [];
+
+const addCard = (card: Card): void => {
+  cards.push(card);
+}
+
+const deleteCard = (card: Card): void => {
+  const index: number = cards.indexOf(card);
+  if (index > -1) {
+    cards.splice(index, 1);
+  }
+}
+
+const getRandomCard = (): Card => {
+  return cards[Math.floor(Math.random() * cards.length)];
+}
 </script>
 
 <template>
-
+  <CardItem v-if="cards && cards.length" v-bind:card="getRandomCard()"></CardItem>
 </template>
 
 <style scoped>
