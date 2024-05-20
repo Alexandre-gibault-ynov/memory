ec58c5f27ad71c3c89934afcbc39cf168e425f00
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-20 17:14:37 +0200

    Add ThemeComponent.vue fields and temporally template

--- src/components/ThemeComponent.vue
+++ src/components/ThemeComponent.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+  import type { Level } from '@/models/Level'
+  import type { Theme } from '@/models/Theme'
+
+  const props = defineProps<{ theme: Theme }>();
+  const levels: Level[] = [];
 
 </script>
 
 <template>
-
+    <h1>{{ theme.name }}</h1>
+    <!-- TODO Display level to review -->
 </template>
 
 <style scoped>
