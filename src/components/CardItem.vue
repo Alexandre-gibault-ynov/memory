b817e35b2058a4f7c6fbd82c7efb977a008e7ec3
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-28 17:54:17 +0200

    Add button to show answer or question, buttons to handle user response to the card question Rework of the Card interface

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,32 +1,55 @@
 <script setup lang="ts">
   import type { Card } from '@/models/Card';
+  import { ref } from 'vue'
 
-  const cardProps = defineProps<{ card: Card }>();
+  const props = defineProps<{ card: Card }>();
+  const emit = defineEmits(['review']);
 
-  const cardEmits = defineEmits<{
-    (e: 'update:verso', face: boolean): void
-  }>();
+  const localCard = ref({ ...props.card });
 
-  const changeFace = () => {
-    cardEmits('update:verso', !cardProps.card.firstFace);
+  const showAnswer = ref(false);
+
+  const handleCorrect = () => {
+    localCard.value.level++;
+    localCard.value.nextReviewDate = calculateNexReviewDate(localCard.value.level);
+    showAnswer.value = false;
+    emit('review', localCard.value);
+  };
+
+  const handleIncorrect = () => {
+    localCard.value.level = 1;
+    localCard.value.nextReviewDate = calculateNexReviewDate(localCard.value.level);
+    showAnswer.value = false;
+    emit('review', localCard.value);
+  }
+
+  const calculateNexReviewDate = (level: number): Date => {
+    const today = new Date();
+    const daysToAdd = Math.pow(2, level - 1);
+    today.setDate(today.getDate() + daysToAdd);
+    return today;
   };
 </script>
 
 <template>
+  <div class="card">
+    <p>{{ localCard.question }}</p>
 
-    <template v-if="card.firstFace">
-      <div @click="changeFace">
-        <h1>{{ card.question }}</h1>
-      </div>
-    </template>
-    <template v-else>
-      <div>
-        <h1>{{ card.answer }}</h1>
-      </div>
-    </template>
+    <button @click="showAnswer = !showAnswer">Afficher la réponse</button>
 
+    <p v-if="showAnswer">{{ localCard.answer }}</p>
+
+    <div v-if="showAnswer">
+      <button @click="handleCorrect">Correct</button>
+      <button @click="handleIncorrect">incorrect</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.card {
+  border: 1px solid #ccc;
+  padding: 1em;
+  margin: 1em;
+}
 </style>
