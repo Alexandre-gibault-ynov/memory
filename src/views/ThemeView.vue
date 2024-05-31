8e5be2e0a0121562c4bfb28a98a8e7421655f1df
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-31 10:37:20 +0200

    Add themeId computedRef

--- src/views/ThemeView.vue
+++ src/views/ThemeView.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 
+  import { useRoute } from 'vue-router'
+  import { computed, type Ref } from 'vue'
+
+  const route = useRoute();
+
+  const themeId = computed(() => route.params.id);
 </script>
 
 <template>
