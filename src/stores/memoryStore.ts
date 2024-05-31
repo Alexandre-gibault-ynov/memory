91928733f5eae666f386d0eb0a55c455b9646648
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-31 11:06:13 +0200

    Add store action addCard

--- src/stores/memoryStore.ts
+++ src/stores/memoryStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import type { ThemeStoreState } from '@/models/ThemeStoreState'
 import type { Theme } from '@/models/Theme'
+import type { Card } from '@/models/Card'
 
 export const useMemoryStore = defineStore('memoryStore', {
   state: (): ThemeStoreState => ({
@@ -9,6 +10,15 @@ export const useMemoryStore = defineStore('memoryStore', {
   actions: {
     addTheme(theme: Theme) {
       this.themes.push(theme);
-    }
+    },
+    addCard(themeId: number, card: Card) {
+      const theme  = this.themes.find(theme => theme.id === themeId);
+      if (theme) {
+        if (!theme.levels[1]) {
+          theme.levels[1] = [];
+        }
+        theme.levels[1].push(card);
+      }
+    },
   }
 });
