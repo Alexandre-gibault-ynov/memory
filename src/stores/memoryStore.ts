df58b7b53bdd449cf45e3efd48673121ff66afce
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-05-31 11:00:26 +0200

    Add store action addTheme

--- src/stores/memoryStore.ts
+++ src/stores/memoryStore.ts
@@ -1,11 +1,14 @@
 import { defineStore } from 'pinia'
 import type { ThemeStoreState } from '@/models/ThemeStoreState'
+import type { Theme } from '@/models/Theme'
 
 export const useMemoryStore = defineStore('memoryStore', {
   state: (): ThemeStoreState => ({
     themes: []
   }),
   actions: {
-
+    addTheme(theme: Theme) {
+      this.themes.push(theme);
+    }
   }
 });
