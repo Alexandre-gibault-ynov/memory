db34e97c5628a0cb0dab39094b8af86bd7436b8f
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-06-19 08:44:39 +0200

    Add method to submit a new theme

--- src/components/ThemeModalComponent.vue
+++ src/components/ThemeModalComponent.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
   import type { Theme } from '@/models/Theme';
   import type { Card } from '@/models/Card';
-  import { ref } from 'vue';
+  import { computed, ref } from 'vue'
   import { useMemoryStore } from '@/stores/memoryStore'
 
   const memoryStore = useMemoryStore();
 
+
+  const storedThemes = computed(() => memoryStore.themes);
   const showModal = ref(false);
   const step = ref(1);
   const newTheme = ref({
@@ -50,6 +52,39 @@
       cards: [{ question: '', answer: '' }]
     };
   };
+
+  /**
+   * Convert the new theme to a {@link ../models/Theme} and
+   * add it to the store themes
+   */
+  const submitTheme = () => {
+    const themeId = storedThemes.value.length + 1;
+    const theme: Theme = {
+      id: themeId,
+      name: '',
+      levels: []
+    };
+    theme.id = themeId;
+    theme.name = newTheme.value.name;
+
+    for (let i = 0; i <= newTheme.value.levelCount; i++) {
+      theme.levels[i] = []
+    }
+    let cardId: number = 1;
+    const initializedCards: Card[] = newTheme.value.cards.map(card => ({
+      id: cardId++,
+      question: card.question,
+      answer: card.answer,
+      level: 1,
+      nextReviewDate: new Date(),
+      themeId: themeId
+    }));
+
+    theme.levels[1] = initializedCards.splice(0, newTheme.value.cardsToAdd);
+    theme.levels[0] = initializedCards.splice(0, initializedCards.length);
+    memoryStore.addTheme(theme);
+    closeModal();
+  }
 </script>
 
 <template>
