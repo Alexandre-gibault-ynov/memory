4935d75569a05c6284855adb4e46d2a9a6dd4161
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-06-19 08:22:59 +0200

    Add step navigation methods

--- src/components/ThemeModalComponent.vue
+++ src/components/ThemeModalComponent.vue
@@ -1,4 +1,33 @@
 <script setup lang="ts">
+  import type { Theme } from '@/models/Theme';
+  import type { Card } from '@/models/Card';
+  import { ref } from 'vue';
+  import { useMemoryStore } from '@/stores/memoryStore'
+
+  const memoryStore = useMemoryStore();
+
+  const showModal = ref(false);
+  const step = ref(1);
+  const newTheme = ref({
+    name: '',
+    levelCount: 0,
+    cardsToAdd: 0,
+    cards: [{ question: '', answer: '' }]
+  });
+
+  /**
+   * Go to step 2 of the modal.
+   */
+  const nextStep = () => {
+    step.value = 2;
+  };
+
+  /**
+   * Go to step 1 of the modal.
+   */
+  const previousStep = () => {
+    step.value = 1;
+  }
 
 </script>
 
